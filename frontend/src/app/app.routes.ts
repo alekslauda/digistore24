f33329fc39b57daa6a8b15516bbb37e02b0354b7
Aleksander Aleksandrov <alekslauda05@gmail.com>
2024-11-16 02:08:45 +0200

    solution

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { AuthFormComponent } from './features/auth/components/auth-form/auth-form.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { ChatListComponent } from './features/chat/components/chat-list/chat-list.component';
+import { GuestGuard } from './core/guards/guest.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthFormComponent,
+    canActivate: [GuestGuard],
+  },
+  {
+    path: 'chat',
+    component: ChatListComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
